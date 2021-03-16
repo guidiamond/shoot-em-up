981646582d42b19407bc2d3f0141adfde28dea36
guidiamond <guilherme.batista770@gmail.com>
2021-03-15 22:02:56 -0300

    shmup package import

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : SteerableBehaviour, IShooter, IDamageable
+{
+  
+
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        float yInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+        Thrust(xInput, yInput);
+    }    
+
+
+    
+}
